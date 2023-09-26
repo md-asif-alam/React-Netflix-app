5f42024eeee75eadbbb5509e1ec772082b16f0b5
Md Asif Alam <mdasifalam910@gmail.com>
2023-09-26 16:41:14 +0530

    first commit

---
+++ src/Sdata.jsx
@@ -0,0 +1,37 @@
+const Sdata = [
+  {
+    imgsrc:
+      "https://occ-0-2794-2219.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABYooBTHs4N3UkCepoM76TAfw7YkFKO37GzRzyticAbdjKNHKkfnkDFc9riKg78FkOzlWCjZp_WTRg2n8HaEiYyIzAK453YTVWl-u.jpg?r=077",
+    title: "A Netflix Original Series",
+    sname: "DARK",
+    link: "https://www.netflix.com/in/title/80100172",
+  },
+  {
+    imgsrc:
+      "https://occ-0-2794-2219.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABS6v2gvwesuRN6c28ZykPq_fpmnQCJwELBU-kAmEcuC9HhWX-DfuDbtA-bfo-IrfgNtxl0qwJJlhI6DENsGFXknKkjhxPGTV-qhp.jpg?r=608",
+    title: "A Netflix Original Series",
+    sname: "Stranger Things",
+    link: "https://www.netflix.com/in/title/80057281",
+  },
+  {
+    imgsrc: "https://i.dawn.com/large/2020/05/5ec697a9ce9b4.jpg",
+    title: "A Netflix Original Series",
+    sname: "Money Heist",
+    link: "https://www.netflix.com/in/title/80192098",
+  },
+  {
+    imgsrc:
+      "https://occ-0-2794-2219.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABYooBTHs4N3UkCepoM76TAfw7YkFKO37GzRzyticAbdjKNHKkfnkDFc9riKg78FkOzlWCjZp_WTRg2n8HaEiYyIzAK453YTVWl-u.jpg?r=077",
+    title: "A Netflix Original Series",
+    sname: "DARK",
+    link: "https://www.netflix.com/in/title/80100172",
+  },
+  {
+    imgsrc: "https://i.dawn.com/large/2020/05/5ec697a9ce9b4.jpg",
+    title: "A Netflix Original Series",
+    sname: "Money Heist",
+    link: "https://www.netflix.com/in/title/80192098",
+  },
+];
+
+export default Sdata;
---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Card from "./Crads";
+import Sdata from "./Sdata";
+
+function ncard(val) {
+  return (
+    <Card
+      imgsrc={val.imgsrc}
+      title={val.title}
+      sname={val.sname}
+      link={val.link}
+    />
+  );
+}
+
+ReactDOM.render(
+  <>
+    <h1 className="heading_style"> List of Top Netflix Web Series </h1>
+    {Sdata.map(ncard)}
+  </>,
+  document.getElementById("root")
+);
