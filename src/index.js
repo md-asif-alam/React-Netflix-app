7ed43673b76a764c7b5d0f8d9e78a970f27141ac
Md Asif Alam <mdasifalam910@gmail.com>
2023-09-26 16:47:31 +0530

    second commit

--- src/index.js
+++ src/index.js
@@ -4,21 +4,30 @@ import "./index.css";
 import Card from "./Crads";
 import Sdata from "./Sdata";
 
-function ncard(val) {
-  return (
-    <Card
-      imgsrc={val.imgsrc}
-      title={val.title}
-      sname={val.sname}
-      link={val.link}
-    />
-  );
-}
+// function ncard(val) {
+//   return (
+//     <Card
+//       imgsrc={val.imgsrc}
+//       title={val.title}
+//       sname={val.sname}
+//       link={val.link}
+//     />
+//   );
+// }
 
 ReactDOM.render(
   <>
     <h1 className="heading_style"> List of Top Netflix Web Series </h1>
-    {Sdata.map(ncard)}
+    {Sdata.map(function ncard(val) {
+      return (
+        <Card
+          imgsrc={val.imgsrc}
+          title={val.title}
+          sname={val.sname}
+          link={val.link}
+        />
+      );
+    })}
   </>,
   document.getElementById("root")
 );
